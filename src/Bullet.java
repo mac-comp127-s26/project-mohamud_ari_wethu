ecf0182e5fb3e07760f3195a5d6c3c8bcdca810e
MohamudBULLE <mbulle@macalester.edu>
2026-04-27 10:16:00 -0500

    Update Bullet.java

--- src/Bullet.java
+++ src/Bullet.java
@@ -2,4 +2,8 @@
 // This will be the bullet that will inherit the Ellipse class from klit graphics
 public class Bullet {
     
+
+    public Bullet (double x, double y, double angle) {
+        
+    }
 }
