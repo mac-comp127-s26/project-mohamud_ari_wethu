11ec2451523b1d1325aa10e499e8d46fb3228091
MohamudBULLE <mbulle@macalester.edu>
2026-04-27 10:14:02 -0500

    Update Gun.java
    Completed Gun class in a way that it uses the bullet class.

--- src/Gun.java
+++ src/Gun.java
@@ -1,6 +1,4 @@
 import edu.macalester.graphics.Line;
-import edu.macalester.graphics.Point;
-
 
 public class Gun {
     // The bullets will be coming out of this gun, which means it will use the bullet class {abstraction}.
@@ -14,32 +12,55 @@ public class Gun {
         if (angleDegrees < 0 || angleDegrees > 180) {
             throw new IllegalArgumentException("angleDegrees must be an angle between 0 and 180 degrees");
         }
-
-        line = new Line(centerX, centerY, 0, 0);
-        line.setStrokeWidth(5);
-        setAngle(angleDegrees);
-
-
-public class Gun {
-    public static final double CANNON_LENGTH = 25;
-
-    private final Line line;
-    private double angle;
-
-    public Gun(double centerX, double centerY, double angleDegrees) {
-        if (angleDegrees < 0 || angleDegrees > 180) {
-            throw new IllegalArgumentException("angleDegrees must be an angle between 0 and 180 degrees");
-        }
-
-        line = new Line(centerX, centerY, 0, 0);
+        line = new Line(centerX, centerY, centerX, centerY-GUN_LENGTH);
         line.setStrokeWidth(5);
         setAngle(angleDegrees);
     }
 
-    private void setAngle(double angle) {
-        this.angle = angle;
+    public Line getLine() {
+        return line;
+    }
 
-        double angleInRadians = Math.toRadians(angle);
-        double x2 = line.getX1() + GUN_LENGTH * Math.cos(angleInRadians);
-        double y2 = line.getY1() + GUN_LENGTH * -Math.sin(angleInRadians);
-        line.setEndPosition(x2, y2);
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setAngle(double angleDegrees) {
+        if (angleDegrees < 0 || angleDegrees > 180) {
+            return;
+        }
+
+        this.angle = angleDegrees;
+
+        double angleRadians = Math.toRadians(angleDegrees);
+
+        double startX = line.getX1();
+        double startY = line.getY1();
+
+        double endX = startX + GUN_LENGTH * Math.cos(angleRadians);
+        double endY = startY - GUN_LENGTH * Math.sin(angleRadians);
+
+        line.setPosition(endX, endY);
+    }
+
+    public void rotateLeft() {
+        setAngle(angle + 5);
+    }
+
+    public void rotateRight() {
+        setAngle(angle - 5);
+    }
+
+    public Bullet shoot() {
+        double angleRadians = Math.toRadians(angle);
+
+        double startX = line.getX1();
+        double startY = line.getY1();
+
+        double endX = startX + GUN_LENGTH * Math.cos(angleRadians);
+        double endY = startY - GUN_LENGTH * Math.sin(angleRadians);
+
+        return new Bullet(endX, endY, angle);
+    }
+}
+
